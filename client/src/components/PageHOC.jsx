a675eb25ebaa9125f9b629426bae53e6689b71d4
panditdhamdhere <dhamdherepandit@gmail.com>
2023-05-14 22:22:42 +0530

    fixing bugs

--- client/src/components/PageHOC.jsx
+++ client/src/components/PageHOC.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+
+import Alert from "./Alert";
+import { useGlobalContext } from "../context";
 import { logo, heroImg } from "../assets";
 import styles from "../styles";
-import { useGlobalContext } from "../context";
-import Alert from "./Alert";
 
 const PageHOC = (Component, title, description) => () => {
   const { showAlert } = useGlobalContext();
   const navigate = useNavigate();
+
   return (
     <div className={styles.hocContainer}>
-{showAlert?.status && <Alert type={showAlert.type} message={showAlert.message} />}
-
+      {showAlert?.status && (
+        <Alert type={showAlert.type} message={showAlert.message} />
+      )}
 
       <div className={styles.hocContentBox}>
         <img
@@ -20,15 +23,20 @@ const PageHOC = (Component, title, description) => () => {
           className={styles.hocLogo}
           onClick={() => navigate("/")}
         />
+
         <div className={styles.hocBodyWrapper}>
           <div className="flex flex-row w-full">
             <h1 className={`flex ${styles.headText} head-text`}>{title}</h1>
           </div>
+
           <p className={`${styles.normalText} my-10`}>{description}</p>
+
           <Component />
         </div>
-        <p className={styles.footerText}>Made with Love 💙 By Pandit</p>
+
+        <p className={styles.footerText}>Made with 💙 By Pandit</p>
       </div>
+
       <div className="flex flex-1">
         <img
           src={heroImg}
